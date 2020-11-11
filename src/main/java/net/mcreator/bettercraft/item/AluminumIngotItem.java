77d8e1cb174a9fb11447a92190d123e67047c8d1
IcaroKaue <73764500+IcaroKaue@users.noreply.github.com>
2020-11-11 20:55:46 -0300

    Added Aluminum Ingot and Aluminum Ore Smelting/Blasting

---
+++ src/main/java/net/mcreator/bettercraft/item/AluminumIngotItem.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.bettercraft.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.bettercraft.BettercraftModElements;
+
+@BettercraftModElements.ModElement.Tag
+public class AluminumIngotItem extends BettercraftModElements.ModElement {
+	@ObjectHolder("bettercraft:aluminum_ingot")
+	public static final Item block = null;
+	public AluminumIngotItem(BettercraftModElements instance) {
+		super(instance, 5);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
+			setRegistryName("aluminum_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
